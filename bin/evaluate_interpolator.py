eeec69791c7e74d813163694dcc6eccd0250ec0b
Elizabeth Champion <elizabethchampion20@gmail.com>
2021-07-12 19:28:28 -0700

    scripts for initial grid generation and interpolator evaluation

---
+++ bin/evaluate_interpolator.py
@@ -0,0 +1,118 @@
+#! /usr/bin/env python3
+
+import numpy as np
+import argparse
+import os
+import sys
+import json
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import RBF, WhiteKernel, ConstantKernel as C
+from scipy.linalg import cholesky, cho_solve
+
+parser = argparse.ArgumentParser(description="Evaluate a single interpolator, specified by `--interp-angle` and `--interp-time`, on the parameters given in `--parameter-file`")
+parser.add_argument("--interp-angle", help="Which angle (of 0, 30, 45, 60, 75, 90) to evaluate")
+parser.add_argument("--interp-time", help="Which time (of the set values at which the interpolators are trained) to use")
+parser.add_argument("--parameter-file", help="Name of file containing parameters to evaluate")
+parser.add_argument("--output-file", help="File to write magnitudes to")
+parser.add_argument("--band", help="Band to evaluate")
+args = parser.parse_args()
+
+# wavelengths corresponding to bands
+wavelengths = {
+        "g":477.56,
+        "r":612.95,
+        "i":748.46,
+        "z":865.78,
+        "y":960.31,
+        "J":1235.0,
+        "H":1662.0,
+        "K":2159.0
+}
+
+# _load_gp() and _model_predict() are stolen from Marko's code
+def _load_gp(fname_base):
+    kernel=None
+    with open(fname_base + ".json",'r') as f:
+        my_json = json.load(f)
+    my_X = np.loadtxt(fname_base + "_X.dat")
+    my_y = np.loadtxt(fname_base + "_y.dat")
+    my_alpha = np.loadtxt(fname_base + "_alpha.dat")
+    dict_params = my_json['kernel_params']
+    theta = np.array(my_json['kernel']).astype('float')
+    theta = np.power(np.e, theta)
+    kernel = WhiteKernel(theta[0]) + theta[1]*RBF(length_scale=theta[2:])
+    gp = GaussianProcessRegressor(kernel=kernel,n_restarts_optimizer=0)
+    gp.kernel_ = kernel
+    dict_params_eval = {}
+    for name in dict_params:
+        if not('length' in name   or 'constant' in name):
+            continue
+        if name =="k2__k2__length_scale":
+            one_space = ' '.join(dict_params[name].split())
+            dict_params_eval[name] = eval(one_space.replace(' ',','))
+        else:
+            dict_params_eval[name] = eval(dict_params[name])
+    gp.kernel_.set_params(**dict_params_eval)
+    gp.X_train_ = my_X
+    gp.y_train_ = my_y
+    gp.alpha_ = my_alpha
+    gp._y_train_std = float(my_json['y_train_std'])
+    gp._y_train_mean = float(my_json['y_train_mean'])
+    return gp
+
+def _model_predict(model, inputs):
+    K = model.kernel_(model.X_train_)
+    K[np.diag_indices_from(K)] += model.alpha
+    model.L_ = cholesky(K, lower=True) # recalculating L matrix since this is what makes the pickled models bulky
+    model._K_inv = None # has to be set to None so the GP knows to re-calculate matrices used for uncertainty
+    K_trans = model.kernel_(inputs, model.X_train_)
+    pred = K_trans.dot(model.alpha_)
+    pred = model._y_train_std * pred + model._y_train_mean
+    v = cho_solve((model.L_, True), K_trans.T)
+    y_cov = model.kernel_(inputs) - K_trans.dot(v)
+    err = np.sqrt(np.diag(y_cov))
+    
+    mags = _log_lums_to_mags(pred)
+    mags_error = 2.5 * err
+
+    return mags, mags_error
+
+def _log_lums_to_mags(log_lums):
+    d = 3.086e18 # parsec in cm
+    d *= 10 # distance of 10 pc
+    log_flux = log_lums - np.log10(4.0 * np.pi * d**2)
+    mags = -48.6 - 2.5 * log_flux
+    return mags
+
+# load the parameters
+params_temp = np.atleast_2d(np.loadtxt(args.parameter_file))
+if params_temp.size == 0:
+    exit()
+
+# store the first column of the array, which contains the parameter indices and should be saved in the output
+indices = params_temp[:,0]
+params = np.empty((params_temp.shape[0], 5))
+params[:,:4] = params_temp[:,1:]
+params[:,4] = wavelengths[args.band]
+
+# location of trained interpolators
+interp_loc = os.environ["INTERP_LOC"]
+if interp_loc[-1] != "/":
+    interp_loc += "/"
+interp_loc += "surrogate_data/2021_Wollaeger_TorusPeanut/"
+
+# load the interpolator
+interp_name = interp_loc + "theta" + ("00" if args.interp_angle == "0" else args.angle) + "deg/t_" + args.interp_time + "_days/model"
+model = _load_gp(interp_name)
+
+# evaluate the interpolator
+mags, mags_err = _model_predict(model, params)
+
+# fill an array with the results
+output_array = np.empty((params.shape[0], 3))
+output_array[:,0] = indices
+output_array[:,1] = mags
+output_array[:,2] = mags_err
+
+# write the output to a file
+np.savetxt(args.output_file, output_array)
---
+++ bin/generate_initial_grid.py
@@ -0,0 +1,67 @@
+#! /usr/bin/env python3
+
+import numpy as np
+from scipy.stats import loguniform, truncnorm
+import argparse
+
+parser = argparse.ArgumentParser(description="Generate initial (random) sample grid")
+parser.add_argument("--fixed-parameter", nargs=2, action="append", help="Fix a parameter's value, e.g. `--fixed-parameter dist 40.0`")
+parser.add_argument("--set-limit", nargs=3, action="append", help="Set a parameter's limits to something other than the default, e.g. `--set-limit mej_dyn 0.01, 0.05`")
+parser.add_argument("--npts", type=int, default=10000, help="Number of points to use in the grid")
+parser.add_argument("--output-file", help="Filename to save grid to")
+# TODO: implement --gaussian-prior option
+args = parser.parse_args()
+
+#
+# functions to draw samples from the possible priors
+#
+def sample_uniform(llim, rlim, n):
+    return np.random.uniform(llim, rlim, size=n)
+
+def sample_log_uniform(llim, rlim, n):
+    return loguniform.rvs(llim, rlim, size=n)
+
+def sample_gaussian(llim, rlim, mu, sigma, n):
+    return truncnorm.rvs(llim, rlim, loc=mu, scale=sigma, size=n)
+   
+# the parameters FIXME: should this be more flexible?
+ordered_parameters = ["mej_dyn", "vej_dyn", "mej_wind", "vej_wind", "theta", "dist"]
+
+# parameter limits
+limits = {
+        "mej_dyn":[0.001, 0.1],
+        "vej_dyn":[0.05, 0.3],
+        "mej_wind":[0.001, 0.1],
+        "vej_wind":[0.05, 0.3],
+        "theta":[0., 90.],
+        "dist":[1., 1000.]
+}
+
+# if the user specified different limits, change them accordingly
+if args.set_limit is not None:
+    for [_parameter, _llim, _rlim] in args.set_limit:
+        limits[_parameter] = [float(_llim), float(_rlim)]
+
+# specify each parameter's prior
+priors = {
+        "mej_dyn":lambda n: sample_log_uniform(*limits["mej_dyn"], n),
+        "vej_dyn":lambda n: sample_uniform(*limits["vej_dyn"], n),
+        "mej_wind":lambda n: sample_log_uniform(*limits["mej_wind"], n),
+        "vej_wind":lambda n: sample_uniform(*limits["vej_wind"], n),
+        "theta":lambda n: sample_uniform(*limits["theta"], n),
+        "dist":lambda n: sample_uniform(*limits["dist"], n)
+}
+
+# deal with possible fixed parameters
+fixed_parameters = {}
+if args.fixed_parameter is not None:
+    for [_parameter, _val] in args.fixed_parameter:
+        fixed_parameters[_parameter] = float(_val)
+
+# do the sampling
+grid = np.empty((args.npts, len(ordered_parameters)))
+for i, _parameter in enumerate(ordered_parameters):
+    grid[:,i] = priors[_parameter](args.npts) if _parameter not in fixed_parameters.keys() else fixed_parameters[_parameter]
+
+# save the grid
+np.savetxt(args.output_file, grid, header=" ".join(ordered_parameters))
